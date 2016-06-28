e122816ea26e6e965b5725d52d10899e68c43abe
Johan Lorenzo <jlorenzo@mozilla.com>
2016-06-28 15:59:33 +0200

    Update test to check if message are sent once and only once

--- test/notifications_test.js
+++ test/notifications_test.js
@@ -5,24 +5,19 @@ const mq = require('zmq').socket('pull');
 const serverManager = require('./server_manager');
 const config = require('./config.js');
 
+function waitForMqMessage() {
+  return new Promise(resolve => {
+    mq.once('message', (message) => {
+      message = JSON.parse(message.toString());
+      // Undeterministic timestamp. Asserting against it doesn't
+      // provide any value to the test
+      delete message.reminder.created;
+      resolve(message);
+    });
+  });
+}
+
 describe('notifications', function() {
-  const reminder = {
-    recipient: 'Jane',
-    message: 'Pick up kids at school',
-    due: Date.now(),
-  };
-
-  const expectedReminder = Object.assign(
-    {
-      id: 1,
-      family: 'family_name',
-      // FIXME: The status value shouldn't be sent to the message queue, as it
-      // gives an outdated information
-      status: 'waiting to be due'
-    },
-    reminder
-  );
-
   const subscription = {
     title: 'my_beautiful_subscription',
     subscription: {
@@ -42,6 +37,38 @@ describe('notifications', function() {
     auth: subscription.subscription.keys.auth
   };
 
+
+  const inputs = [{
+    recipient: 'Jane',
+    message: 'Pick up kids at school',
+    due: Date.now(),
+  }, {
+    recipient: 'John',
+    message: 'Buy milk',
+    due: Date.now(),
+  }, {
+    recipient: 'David',
+    message: 'Go to school',
+    due: Date.now(),
+  }];
+
+  const outputs = inputs.map((input, i) => {
+    const reminder = Object.assign(
+      {
+        id: i + 1,
+        family: 'family_name',
+        // FIXME: The status value shouldn't be sent to the message queue, as it
+        // gives an outdated information
+        status: 'waiting'
+      },
+      input
+    );
+    return {
+      reminder,
+      subscription: expectedSubscription
+    };
+  });
+
   const remindersUrl = `${config.apiRoot}/reminders`;
   const subscriptionsUrl = `${config.apiRoot}/subscriptions`;
 
@@ -49,33 +76,29 @@ describe('notifications', function() {
 
   serverManager.inject();
 
-  beforeEach(function() {
+  beforeEach(function*() {
     mq.connect(mqSocket);
+    yield chakram.post(subscriptionsUrl, subscription);
   });
 
   afterEach(function() {
     mq.disconnect(mqSocket);
   });
 
-  it('schedules notifications', function() {
-    return Promise.all([
-      chakram.post(remindersUrl, reminder),
-      chakram.post(subscriptionsUrl, subscription),
-    ]).then(() =>
-      new Promise(resolve => {
-        mq.once('message', (message) => {
-          resolve(message);
-        });
-      })
-    ).then(message => {
-      message = JSON.parse(message.toString());
-      // Undeterministic timestamp. Asserting against it doesn't
-      // provide any value to the test
-      delete message.reminder.created;
-      expect(message).deep.equal({
-        reminder: expectedReminder,
-        subscription: expectedSubscription
-      });
-    });
+  it('should not send a reminder twice to the message queue', function*() {
+    this.timeout(10000); // Test is longer than the regular 2s-allowed-span.
+
+    // We try to determine if the first reminder has been sent only once.
+    // In order to do so, we verify that this reminder is not sent a second time
+    // in the second setInterval. As our SQL queries are not ordered, it might
+    // occur that the first reminder pops up after the second one (in the second
+    // interval). Hence, the 3rd reminder is a sentinel to make sure we entered
+    // a new interval.
+    for (let i = 0; i < inputs.length; i++) {
+      yield chakram.post(remindersUrl, inputs[i]);
+
+      const message = yield waitForMqMessage();
+      expect(message).deep.equal(outputs[i]);
+    }
   });
 });
