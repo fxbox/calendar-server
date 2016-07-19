9c70211d424b44551e59d076e7eadf73f51d7458
Johan Lorenzo <jlorenzo@mozilla.com>
2016-07-19 17:40:24 +0200

    fix nits in wait.js

--- test/lib/wait.js
+++ test/lib/wait.js
@@ -1,18 +1,15 @@
 const until = require('until-promise').default;
 const { profilePath } = require('../server_manager');
 
-
 const maxDurationInMs = 5000;
 const intervalInMs = 500;
 
-const debug = require('debug')('calendar-server:wait');
-
-
 function waitUntilReminderHasStatus(family, id, status) {
+  // We connect to the database here, so we don't try to connect to a
+  // non-existing one. Test database are often deleted between tests.
   require('../../dao/database').init(profilePath);
   const dao = require('../../dao/reminders');
 
-  debug('here');
   return until(
     () => dao.show(family, id),
     (reminder) => reminder.status === status,
